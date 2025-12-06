19e39e9ab8e83fa79436488c2d72306f776fca65
yamaguti-Hikaru <hikaru171102@icloud.com>
2025-12-06 23:49:19 +0900

    first commit

---
+++ project1/src/abc/hello.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var m = map[string]int{"A":100,"B":200}
+	fmt.Println(m)
+
+	m2 := map[string]int{"A":100,"B":200}
+	fmt.Println(m2)
+
+	m3 := map[int]string{
+		1:"a",
+		2:"b",
+	}
+	fmt.Println(m3)
+
+	m4 := make(map[int]string)
+	fmt.Println(m4)
+
+	m4[1] = "japan"
+	m4[2] = "usa"
+	fmt.Println(m4)
+
+	fmt.Println(m["A"])
+	fmt.Println(m4[1])
+	fmt.Println(m4[2], m3[1])
+
+	s, ok := m4[3]
+	if !ok {
+		fmt.Println("error")
+	}
+	fmt.Println(s, ok)
+
+	m4[2] = "us"
+	fmt.Println(m4)
+
+	m4[3] = "china"
+	fmt.Println(m4)
+
+	fmt.Println(len(m4))
+
+	delete(m4, 3)
+	fmt.Println(m4)
+
+	fmt.Println(len(m4))
+}
