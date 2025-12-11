f5784056df146489411226ea105e0eadd9b1a8eb
yamaguti-Hikaru <hikaru171102@icloud.com>
2025-12-11 23:49:47 +0900

    構造体のポインタを格納するスライスの使用

--- project1/src/abc/hello.go
+++ project1/src/abc/hello.go
@@ -2,58 +2,40 @@ package main
 
 import (
 	"fmt"
-
 )
 
 type User struct {
 	Name string
 	Age  int
-	//x, y int
 }
 
-func UpdateUser(user User) User {
-	user.Name = "A"
-	user.Age = 1000
-	return user
+type Users []*User
+/*
+type users struct {
+	Users []*Users
 }
+*/
 
-func UpdateUser2(user *User) {
-	user.Name = "A"
-	user.Age = 1000
-}
 func main() {
-	var user1 User
-	fmt.Println(user1)
-	user1.Name = "user1"
-	user1.Age = 30
-	fmt.Println(user1)
+	user1 := User{Name: "user1", Age: 10}
+	user2 := User{Name: "user1", Age: 20}
+	user3 := User{Name: "user1", Age: 30}
+	user4 := User{Name: "user1", Age: 40}
 
-	user2 := User{}
-	fmt.Println(user2)
-	user2.Name = "user2"
-	fmt.Println(user2)
-	
-	user3 := User{Name: "user3", Age: 25}
-	fmt.Println(user3)
+	users := Users{}
 
-	user4 := User{"user4", 28}
-	fmt.Println(user4)
+	users = append(users,&user1)
+	users = append(users,&user2)
+	users = append(users,&user3, &user4)
 
-	//user5 := User{30, "user5"}
-	//fmt.Println(user5)
+	for _, u := range users{
+		fmt.Println(*u)
+	}
 
-	user6 := &User{Name: "user6"}
-	fmt.Println(user6)
+	users2 := make([]*User, 0)
+	users2 = append(users2,  &user1, &user2)
 
-	user7 := new(User)
-	fmt.Println(user7)
-
-	user8 := &User{}
-	fmt.Println(user8)
-
-	UpdateUser(user1)
-	UpdateUser2(user8)
-
-	fmt.Println(user1)
-	fmt.Println(user8)
+	for _, u := range users2{
+		fmt.Println(*u)
+	}
 }
