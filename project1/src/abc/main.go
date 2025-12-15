752708a2d28594502fd6cf8c6ef1df092e40e55b
yamaguti-Hikaru <hikaru171102@icloud.com>
2025-12-15 23:00:53 +0900

    a

--- project1/src/abc/main.go
+++ project1/src/abc/main.go
@@ -12,5 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
   http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
   http.ListenAndServe(":8080", nil)
+
+  fmt.Println("a")
 }
 
